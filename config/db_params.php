59eb231517b0d22de42b95d5362adc2a220cff70
RomanKornei <31906125+RomanKornei@users.noreply.github.com>
2017-09-14 19:17:21 +0300

    Update db_params.php

--- config/db_params.php
+++ config/db_params.php
@@ -1,7 +1,7 @@
 <?php
 // Массив с параметрами подключения к базе данных
 return array(
-	'host' => '127.0.0.1',
+	'host' => '127.0.0.0',
 	'dbname' => 'Users',
 	'user' => 'root',
 	'password' => '',
