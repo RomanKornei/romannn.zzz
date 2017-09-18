a799b16b1cdeedee35e99182134dd39dd2cafaad
RomanKornei <korneevroman94@mail.com>
2017-09-18 07:10:28 +0300

    Delet folder

--- template/views/layouts/web.php
+++
@@ -1,8 +0,0 @@
-		<div id="web_sites">
-				<span>Тут может быть Ваша реклама</span>
-				<a href="/ad/" title="Заказать сайт">
-						<div class="btn">
-							Подробнее
-						</div>
-				</a>
-			</div>
